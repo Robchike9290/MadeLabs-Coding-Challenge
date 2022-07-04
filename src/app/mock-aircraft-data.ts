b2b76512fb2403448980ea7cc3efb03ab6d9420f
Robert Czajka <robchike9290@gmail.com>
2022-07-04 13:09:37 -0500

    Add in mock data from challenge description with comments for data types to be displayed upon retrieval.

---
+++ src/app/mock-aircraft-data.ts
@@ -0,0 +1,59 @@
+const aircraft = [
+  {
+    AircraftId: 1,
+    DailyHours: 0.7,
+    CurrentHours: 550
+  },
+  {
+    AircraftId: 2,
+    DailyHours: 1.1,
+    CurrentHours: 200
+  }
+]
+
+// TASKS //
+
+// Data Types: //
+
+// ItemNumber: int
+// Description: string
+// LogDate: date
+// LogHours: int(Null)
+// IntervalMonths: int(Null)
+// IntervalHours: int(Null)
+// NextDue: date(Null)
+
+const tasks = [
+  {
+    ItemNumber: 1,
+    Description: "Item 1",
+    LogDate: "2018-04-07T00:00:00",
+    LogHours: null,
+    IntervalMonths: null,
+    IntervalHours: null
+  },
+  {
+    ItemNumber: 2,
+    Description: "Item 2",
+    LogDate: "2018-04-07T00:00:00",
+    LogHours: 100,
+    IntervalMonths: 12,
+    IntervalHours: 500
+  },
+  {
+    ItemNumber: 3,
+    Description: "Item 3",
+    LogDate: "2018-06-01T00:00:00",
+    LogHours: 150,
+    IntervalMonths: null,
+    IntervalHours: 400
+  },
+  {
+    ItemNumber: 4,
+    Description: "Item 4",
+    LogDate: "2018-06-01T00:00:00",
+    LogHours: 150,
+    IntervalMonths: 6,
+    IntervalHours: null
+  }
+];
