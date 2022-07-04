f124d088ae7a49649de57fbf3a5e060144d381bb
Robert Czajka <robchike9290@gmail.com>
2022-07-04 13:02:07 -0500

    Remove boilerplate code from HTML and replace with app title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-madelabs-coding-challenge';
+  title = 'Aircraft Maintenance Schedule Tracker';
 }
