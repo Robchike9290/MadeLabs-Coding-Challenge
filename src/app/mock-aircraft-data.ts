43e129d35e4c6b53477c817cd60b44aa897b3dd8
Robert Czajka <robchike9290@gmail.com>
2022-07-04 18:17:15 -0500

    Create interfaces for aircraft and tasks, correct spelling of properties to camel case.

--- src/app/mock-aircraft-data.ts
+++ src/app/mock-aircraft-data.ts
@@ -1,59 +1,63 @@
-const aircraft = [
+export interface Aircraft {
+  aircraftId: number,
+  dailyHours: number,
+  currentHours: number
+}
+
+export const aircraft: Aircraft[] = [
   {
-    AircraftId: 1,
-    DailyHours: 0.7,
-    CurrentHours: 550
+    aircraftId: 1,
+    dailyHours: 0.7,
+    currentHours: 550
   },
   {
-    AircraftId: 2,
-    DailyHours: 1.1,
-    CurrentHours: 200
+    aircraftId: 2,
+    dailyHours: 1.1,
+    currentHours: 200
   }
 ]
 
-// TASKS //
+export interface Task {
+  itemNumber: number,
+  description: string,
+  logDate: Date,
+  logHours: number | null,
+  intervalMonths: number | null,
+  intervalHours: number | null,
+  nextDue?: number | null
+}
 
-// Data Types: //
-
-// ItemNumber: int
-// Description: string
-// LogDate: date
-// LogHours: int(Null)
-// IntervalMonths: int(Null)
-// IntervalHours: int(Null)
-// NextDue: date(Null)
-
-const tasks = [
+export const tasks: Task[] = [
   {
-    ItemNumber: 1,
-    Description: "Item 1",
-    LogDate: "2018-04-07T00:00:00",
-    LogHours: null,
-    IntervalMonths: null,
-    IntervalHours: null
+    itemNumber: 1,
+    description: "Item 1",
+    logDate: new Date("2018-04-07T00:00:00"),
+    logHours: null,
+    intervalMonths: null,
+    intervalHours: null
   },
   {
-    ItemNumber: 2,
-    Description: "Item 2",
-    LogDate: "2018-04-07T00:00:00",
-    LogHours: 100,
-    IntervalMonths: 12,
-    IntervalHours: 500
+    itemNumber: 2,
+    description: "Item 2",
+    logDate: new Date("2018-04-07T00:00:00"),
+    logHours: 100,
+    intervalMonths: 12,
+    intervalHours: 500
   },
   {
-    ItemNumber: 3,
-    Description: "Item 3",
-    LogDate: "2018-06-01T00:00:00",
-    LogHours: 150,
-    IntervalMonths: null,
-    IntervalHours: 400
+    itemNumber: 3,
+    description: "Item 3",
+    logDate: new Date("2018-06-01T00:00:00"),
+    logHours: 150,
+    intervalMonths: null,
+    intervalHours: 400
   },
   {
-    ItemNumber: 4,
-    Description: "Item 4",
-    LogDate: "2018-06-01T00:00:00",
-    LogHours: 150,
-    IntervalMonths: 6,
-    IntervalHours: null
+    itemNumber: 4,
+    description: "Item 4",
+    logDate: new Date("2018-06-01T00:00:00"),
+    logHours: 150,
+    intervalMonths: 6,
+    intervalHours: null
   }
 ];
