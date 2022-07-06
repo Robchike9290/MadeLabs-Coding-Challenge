1197edcfcd5f677a388b5854796aafdb0f44eb48
Robert Czajka <robchike9290@gmail.com>
2022-07-05 22:07:21 -0500

    Create function to update intervalMonthsNextDueDate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { observable, computed, action } from 'mobx-angular';
 import { Aircraft } from './mock-aircraft-data';
 import { AircraftService } from './aircraft.service';
 import { Task } from './mock-aircraft-data';
@@ -10,10 +11,25 @@ import { TaskService } from './task.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
   aircraft: Aircraft[] = [];
-  selectedAircraft?: number | null = null;
+  selectedAircraft: number = 1;
   tasks: Task[] = [];
   title = 'Aircraft Maintenance Schedule Tracker';
+
+  @computed getCurrentHours(): number {
+    return this.aircraft[this.selectedAircraft - 1].currentHours;
+  }
+
+  @action setIntervalMonthsNextDueDate(taskNumber: number): Date | null {
+    const logDate: Date | null = this.tasks[taskNumber - 1].logDate;
+    const intervalMonths: number | null = this.tasks[taskNumber - 1].intervalMonths;
+    let intervalMonthsNextDueDate: Date | null = null;
+    if (intervalMonths) {
+      intervalMonthsNextDueDate =  new Date(logDate.setMonth(logDate.getMonth() + intervalMonths));
+    }
+    return intervalMonthsNextDueDate;
+  }
 
   constructor(
     private aircraftService: AircraftService,
